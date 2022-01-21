573522ff973c3fbfb0c14cbe38eb932e82f12b88
Joao Victor <joaovictordeveloper@outlook.com>
2022-01-21 01:02:34 -0300

    tela de SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Platform,
+  KeyboardAvoidingView
+} from "react-native";
 
-import { Container } from './styles';
+import brandImg from '../../assets/png/brand.png'
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { 
+  Container, 
+  Content, 
+  Brand,
+  Title,
+  ForgotPasswordButton,
+  ForgotPasswordLabel 
+} from './styles';
 
 export function SignIn() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <Container>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
 
+        <Content>
+          <Brand source={brandImg} />
+          <Title>Login</Title>
+
+          <Input
+            type="secondary"
+            placeholder="E-mail"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+
+          <Input
+            type="secondary"
+            placeholder="Senha"
+            secureTextEntry
+          />
+
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button
+            title="Entrar"
+            type="secondary"
+            isLoaded={isLoaded}
+          />
+        </Content>
+
+      </KeyboardAvoidingView>
     </Container>
   )
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,10 +1,48 @@
 import styled from "styled-components/native";
 import { LinearGradient } from 'expo-linear-gradient';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
+export const Content = styled.ScrollView.attrs({
+  showVerticalScrollIndicator: false,
+  contentContainerStyle: { paddingBottom: getBottomSpace() + 18 }
+})`
+  width: 100%;
+  padding: 0 32px;
+`;
+
+export const Brand = styled.Image.attrs({
+  resizeMode: 'contain'
+})`
+  height: 340px;
+  margin: 64px 0 32px;
+  align-self: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 32px;
+  font-family: ${({ theme }) => theme.FONTS.TITLE};
+  color: ${({ theme }) => theme.COLORS.TITLE}
+  
+  align-self: flex-start;
+  margin-bottom: 24px;
+`;
+
+export const ForgotPasswordButton = styled.TouchableOpacity`
+  align-self: flex-end;
+  margin-bottom: 20px;
+`;
+
+export const ForgotPasswordLabel = styled.Text`
+  font-size: 14px;
+  font-family: ${({ theme }) => theme.FONTS.TEXT};
+  color: ${({ theme }) => theme.COLORS.TITLE};
+`;
+
 
 export const Container = styled(LinearGradient).attrs(({ theme }) => ({
   colors: theme.COLORS.GRADIENT,
   start: { x: 0, y: 1 },
-  end: { x: 0.5, y: 0.5}
+  end: { x: 0.5, y: 0.5 },
 }))`
   flex: 1;
   justify-content: center;
