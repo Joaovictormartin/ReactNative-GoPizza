ff4266c34775cdc02244d70826888d4edf9e779d
Joao Victor <joaovictordeveloper@outlook.com>
2022-01-21 13:56:19 -0300

    configurando a funcionalidade de carregar a imagem pelo button carregar

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Platform } from "react-native";
+import * as ImagePicker from "expo-image-picker";;
 
 import { Photo } from "../../components/Photo";
 import { Input } from "../../components/Input";
@@ -17,6 +18,24 @@ import {
 } from './styles';
 
 export function Product() {
+  const [image, setImage] = useState('');
+
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if(status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [ 4, 4 ]
+      });
+
+      if(!result.cancelled) {
+        //@ts-ignore
+        setImage(result.uri)
+      }
+    }
+  }
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
@@ -28,11 +47,11 @@ export function Product() {
       </Header>
 
       <Upload>
-        <Photo uri=""/>
+        <Photo uri={image}/>
         <PickerImagemButton 
           type="secondary" 
           title="Carregar" 
-          onPress={() =>{}}
+          onPress={handlePickerImage}
         />
       </Upload>
 
