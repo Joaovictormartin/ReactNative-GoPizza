9202c80b93a32966b841fe29ccff29d87d94d75c
Joao Victor <joaovictordeveloper@outlook.com>
2022-01-25 20:37:33 -0300

    Exibindo pizzas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
-import { Platform, Alert } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Platform, Alert, FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
 
 import happyEmojis from "../../assets/png/happy.png";
 
 import { useAuth } from "../../hooks/useAuth";
 import { Search } from "../../components/Search";
 import { Button } from "../../components/Button";
+import { ProductCard, ProductProps } from "../../components/ProductCard";
 
 import {
   Container,
@@ -17,36 +19,91 @@ import {
   SignOutIcon,
   MenuHeader,
   MenuTitle,
-  MenuItemsNumber
+  MenuItemsNumber,
 } from "./styles";
 
 export function Home() {
   const { user, SignOut } = useAuth();
 
+  const [ pizzas, setPizzas ] = useState<ProductProps[]>([])
+
+  const data = [
+    {
+      id: "1",
+      name: "Pizza",
+      description: "Ingredientes dessa pizza",
+      photo_url: "https://github.com/joaovictormartin.png",
+    },
+    {
+      id: "2",
+      name: "Pizza",
+      description: "Ingredientes dessa pizza",
+      photo_url: "https://github.com/joaovictormartin.png",
+    },
+  ];
+
+  function fecthPizza(value: string) {
+    const formattedValue = value.toLocaleLowerCase().trim();
+
+    firestore()
+      .collection("pizzas")
+      .orderBy("name_insensitive")
+      .startAt(formattedValue)
+      .endAt(`${formattedValue}\uf8ff`)
+      .get()
+      .then((response) => {
+        const data = response.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+
+        setPizzas(data);
+      })
+      .catch(() =>
+        Alert.alert("Consulta", "Não foi possível realizar a consulta")
+      );
+  }
+
   function handleSignOut() {
     SignOut();
   }
 
+  useEffect(() => {
+    fecthPizza('')
+  },[])
+
   return (
-    <Container >
+    <Container>
       <Header>
         <Greeting>
-          <GreetingEmoji source={happyEmojis}/>
+          <GreetingEmoji source={happyEmojis} />
           <GreetingText>Olá, {user?.name}</GreetingText>
         </Greeting>
 
         <SignOutContainer onPress={handleSignOut}>
-          <SignOutIcon name="logout"/>
+          <SignOutIcon name="logout" />
         </SignOutContainer>
       </Header>
 
-      <Search onSearch={() => {}} onClear={() => {}}/>
+      <Search onSearch={() => {}} onClear={() => {}} />
 
       <MenuHeader>
         <MenuTitle>Cardápio</MenuTitle>
         <MenuItemsNumber>32 pizzas</MenuItemsNumber>
       </MenuHeader>
-      
+
+      <FlatList
+        data={pizzas}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <ProductCard data={item} />}
+        contentContainerStyle={{ 
+          paddingTop: 20, 
+          paddingBottom: 125,
+          marginHorizontal: 24, 
+        }}
+      />
     </Container>
   );
 }
