350e4a1b774fac3d1866a844e20c2aee2ad4fe79
Joao Victor <joaovictordeveloper@outlook.com>
2022-01-26 18:57:15 -0300

    Header do pedido

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import { Platform } from "react-native";
+//import { useNavigation } from "@react-navigation/native";
 
-import { Container } from "./styles";
+import { ButtonBack } from "../../components/ButtonBack";
+
+import { 
+  Container,
+  Header 
+} from "./styles";
 
 export function Order() {
+  //const { navigate, goBack } = useNavigation();
+
   return (
     <Container
       behavior={Platform.OS === "ios" ? "padding" : undefined}
     >
-      
+      <Header>
+        <ButtonBack 
+          onPress={() => {}}
+          style={{ marginBottom: 108 }}
+        />
+      </Header>
     </Container>
   );
 }
--- src/screens/Order/styles.ts
+++ src/screens/Order/styles.ts
@@ -1,5 +1,16 @@
 import styled from "styled-components/native";
+import { LinearGradient } from "expo-linear-gradient";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.KeyboardAvoidingView`
   flex: 1;
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme }) => ({
+  colors: theme.COLORS.GRADIENT,
+}))`
+  width: 100%;
+  height: 204px;
+
+  padding: ${getStatusBarHeight() + 34}px 24px 0;
 `;
