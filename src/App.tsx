65a80ddd55d7c9c5dff2a1b7ca19a7b7e1650a45
Joao Victor <joaovictordeveloper@outlook.com>
2022-01-25 21:04:39 -0300

    add a routes no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { StatusBar } from 'expo-status-bar';
-import { ThemeProvider } from 'styled-components/native';
-import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
-import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import React from "react";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
+import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 
-import theme from './theme';
-import { AppProvider } from './hooks'
+import theme from "./theme";
+import { Routes } from './routes';
+import { AppProvider } from "./hooks";
 
-import { SignIn } from './screens/SignIn';
+import { Home } from "./screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
-    DMSerifDisplay_400Regular
-  })
+    DMSerifDisplay_400Regular,
+  });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style='light' translucent backgroundColor='transparent' />
+      <StatusBar style="light" translucent backgroundColor="transparent" />
 
       <AppProvider>
-        <SignIn />
+        <Routes />
       </AppProvider>
     </ThemeProvider>
   );
