017cef846cdbdca312069ac6b98e177b967be1fe
Joao Victor <joaovictordeveloper@outlook.com>
2022-01-26 18:48:17 -0300

    Interface de pedido

---
+++ src/screens/Order/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Platform } from "react-native";
+
+import { Container } from "./styles";
+
+export function Order() {
+  return (
+    <Container
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      
+    </Container>
+  );
+}
