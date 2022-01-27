3b150234f9fb1343431ea174ef0497d7c87579b7
Joao Victor <joaovictordeveloper@outlook.com>
2022-01-27 20:24:38 -0300

    add a routes no App

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+import { useAuth } from '../hooks/useAuth';
+
 import { UserStackRoutes } from './user.stack.routes';
+import { UserTabRoutes } from './user.tab.routes';
 
 export function Routes() {
+  const { user } = useAuth();
   return(
     <NavigationContainer>
-      <UserStackRoutes/>
+      <UserTabRoutes/>
+      {/* {user ? <UserTabRoutes/> : <UserStackRoutes/>} */}
     </NavigationContainer>
   )
 }
