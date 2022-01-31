8b64442d34ac61dee3338208ad2096a01607f2d0
Joao Victor <joaovictordeveloper@outlook.com>
2022-01-31 13:56:02 -0300

    puxando os dados do firebase

--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -1,33 +1,67 @@
-import React, { useState } from "react";
-import { FlatList } from "react-native"
-//import { useNavigation } from "@react-navigation/native";
+import React, { useState, useEffect } from "react";
+import { Alert, FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+import { useNavigation } from "@react-navigation/native";
 
+import { useAuth } from "../../hooks/useAuth";
 import { OrderCard } from "../../components/OrderCard";
 
-import {
-  Container,
-  Header,
-  Title,
-  Separator
-} from "./styles";
+import { Container, Header, Title, Separator } from "./styles";
 
 export function Orders() {
-  //const { navigate, goBack } = useNavigation();
+  const { user } = useAuth();
+  const { navigate, goBack } = useNavigation();
 
-  function renderItem({item, index}) {
-    return(
+  const [orders, setOrders] = useState([]);
+
+  function handlePizzaDelivery(id: string) {
+    Alert.alert("Pedido", "Confirmar que a pizza foi entregue?", [
+      {
+        text: "Não",
+        style: "cancel",
+      },
+      {
+        text: "Sim",
+        onPress: () => {
+          firestore().collection("orders").doc(id).update({
+            status: "Entregue",
+          });
+        },
+      },
+    ]);
+  }
+
+  function renderItem({ item, index }) {
+    return (
       <OrderCard
         index={index}
-        name="4Queijos"
-        description="teste"
-        status="Preparando"
+        data={item}
+        disabled={item.status === "Entregue"}
+        onPress={() => handlePizzaDelivery(item.id)}
       />
-    )
+    );
   }
 
   function ItemSeparator() {
-    return <Separator/>
+    return <Separator />;
   }
+
+  useEffect(() => {
+    const subscribe = firestore()
+      .collection("orders")
+      .where("waiter_id", "==", user?.id)
+      .onSnapshot((querySnapshot) => {
+        const data = querySnapshot.docs.map((docs) => {
+          return {
+            id: docs.id,
+            ...docs.data(),
+          };
+        });
+        setOrders(data);
+      });
+
+    return () => subscribe();
+  }, []);
 
   return (
     <Container>
@@ -36,18 +70,17 @@ export function Orders() {
       </Header>
 
       <FlatList
-        data={['1', '2' , '3', '4', '5', '6']}
-        keyExtractor={item => item}
+        data={orders}
+        keyExtractor={(item) => item.id}
         numColumns={2}
         renderItem={renderItem}
         showsVerticalScrollIndicator={false}
         ItemSeparatorComponent={ItemSeparator}
-        contentContainerStyle={{ 
+        contentContainerStyle={{
           paddingBottom: 125,
-          paddingHorizontal: 24
+          paddingHorizontal: 24,
         }}
       />
-
     </Container>
   );
 }
