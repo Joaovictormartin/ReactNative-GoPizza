50f3cae5569c348795c3c17292647a6ef64e7114
Joao Victor <joaovictordeveloper@outlook.com>
2022-01-31 14:22:58 -0300

    Pedidos em aberto na tabBar

--- src/routes/user.tab.routes.tsx
+++ src/routes/user.tab.routes.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Platform } from "react-native";
 import { useTheme } from "styled-components/native";
+import firestore from "@react-native-firebase/firestore";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { BottomMenu } from '../components/BottomMenu';
+import { BottomMenu } from "../components/BottomMenu";
 
 import { Home } from "../screens/Home";
 import { Orders } from "../screens/Orders";
@@ -12,6 +13,19 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 export function UserTabRoutes() {
   const { COLORS } = useTheme();
+
+  const [notification, setNotification] = useState("0");
+
+  useEffect(() => {
+    const subscribe = firestore()
+      .collection("orders")
+      .where("status", "==", "Pronto")
+      .onSnapshot((querySnapshot) => {
+        setNotification(String(querySnapshot.docs.length));
+      });
+    
+    return () => subscribe();
+  }, []);
 
   return (
     <Navigator
@@ -26,22 +40,26 @@ export function UserTabRoutes() {
         },
       }}
     >
-      <Screen 
-        name="home" 
+      <Screen
+        name="home"
         component={Home}
         options={{
           tabBarIcon: ({ color }) => (
             <BottomMenu title="Cardápio" color={color} />
-          )
+          ),
         }}
       />
-      <Screen 
-        name="orders" 
+      <Screen
+        name="orders"
         component={Orders}
         options={{
           tabBarIcon: ({ color }) => (
-            <BottomMenu title="Pedidos" color={color} notification="1"/>
-          )
+            <BottomMenu
+              title="Pedidos"
+              color={color}
+              notification={notification}
+            />
+          ),
         }}
       />
     </Navigator>
